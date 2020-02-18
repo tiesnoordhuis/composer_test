afc443030f32cc6c8535c3451d9d0ca34546b81c
ties noordhuis <tiesnoordhuis@gmail.com>
2020-02-18 16:48:37 +0100

    trying to add logging to bindmount

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
 FROM php:7.2-apache
+# RUN addgroup --gid 1012 multiplemy
+# RUN adduser --disabled-password --gecos "" --force-badname --gid 1012 dockermy
+# USER dockermy
+
+COPY vendor/ /var/www/vendor
 COPY src/ /var/www/html
 EXPOSE 80
+
+
+# run command: docker run -v /home/ties/projects/personal/php/composer_test/logs:/var/log/nginx -p 80:80 my-php-app
--- src/main.php
+++ src/main.php
@@ -1,7 +1,28 @@
 <?php
 
-echo("hello world" . PHP_EOL);
+require '../vendor/autoload.php';
 
+echo("hello world" . PHP_EOL);
+echo("<br>");
+echo(implode("; ", scandir("../../log/nginx/")));
+echo ("<br>");
+echo(substr(sprintf('%o', fileperms('../../log/nginx/')), -4));
+echo ("<br>");
+echo(file_get_contents("../../log/nginx/main.log") . PHP_EOL);
+echo ("<br>");
+
+// echo(phpinfo());
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../../log/nginx/main.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
 
 
 ?>
