29e003c04e5703a79236909b6a15594b9420a263
ties noordhuis <tiesnoordhuis@gmail.com>
2020-02-18 17:10:55 +0100

    working version that logs to main.log from php

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM php:7.2-apache
-# RUN addgroup --gid 1012 multiplemy
-# RUN adduser --disabled-password --gecos "" --force-badname --gid 1012 dockermy
-# USER dockermy
+RUN addgroup --gid 1012 multiplemy
+RUN usermod -a -G 1012 www-data
 
 COPY vendor/ /var/www/vendor
 COPY src/ /var/www/html
 EXPOSE 80
 
 
-# run command: docker run -v /home/ties/projects/personal/php/composer_test/logs:/var/log/nginx -p 80:80 my-php-app
+# run command: docker run -v /home/ties/projects/personal/php/composer_test/logs:/var/www/logs -p 80:80 my-php-app
--- src/main.php
+++ src/main.php
@@ -3,22 +3,14 @@
 require '../vendor/autoload.php';
 
 echo("hello world" . PHP_EOL);
-echo("<br>");
-echo(implode("; ", scandir("../../log/nginx/")));
-echo ("<br>");
-echo(substr(sprintf('%o', fileperms('../../log/nginx/')), -4));
-echo ("<br>");
-echo(file_get_contents("../../log/nginx/main.log") . PHP_EOL);
-echo ("<br>");
-
-// echo(phpinfo());
+echo(implode("; ", scandir("../")));
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler('../../log/nginx/main.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('../logs/main.log', Logger::WARNING));
 
 // add records to the log
 $log->warning('Foo');
